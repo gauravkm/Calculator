7068c7a4f51626a13cb95cac1209e065b688d44c
Gaurav Mittal <gaurav.mi@directi.com>
2015-05-10 17:04:11 +0530

    Replace labels with buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UITextField!
+    
+    var calculatorBrain = CalculatorBrain()
+    var inTheMiddleOfTypingANumber = false
+    
+    @IBAction func numberClick(sender: UIButton) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func returnClick() {
     }
-
-
+    
+    @IBAction func performOperation(sender: UIButton) {
+    }
+    
 }
 
